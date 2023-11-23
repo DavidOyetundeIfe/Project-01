9b85f02e01418370ebbedc0382571a4194702f31
David Oyetunde <oyeytundedvd101@gmail.com>
2023-11-23 08:00:46 +0100

    Version 13

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -6,12 +6,16 @@ import { useOutletContext } from "react-router-dom";
 
 function Details() {
     const {currentVan} = useOutletContext();
-    return  <>  
-    <div>
-    <h1>Details welcomes you</h1>
-    </div>
-   
-     </>
+    return (
+
+        <section>
+            <h4>Name: {currentVan.name}</h4>
+            <h4>Category: {currentVan.type}</h4>
+            <h4>Description: {currentVan.description}</h4>
+            <h4>Visibility: public</h4>
+        </section>
+     )
+  
 }
 
 export default Details;
