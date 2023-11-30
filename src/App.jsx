227de9e2b45acea1a43bb2c43d12e6d5a9e1c899
David Oyetunde <oyeytundedvd101@gmail.com>
2023-11-30 11:50:35 +0100

    version 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import './App.css';
+import { Outlet } from "react-router-dom";
 
 
 
 function App() {
+
+  const [count, Setcount] = React.useState(0);
   
   return ( 
   <>
@@ -11,6 +14,7 @@ function App() {
         <br/><br/><br/><br/><br/>
         <img src="../c1.JPG" alt="house" className="img"/>
         <h4>I am on the fucking moon so let us get the fuck out of the place right now or we will not get to where we are going .</h4>
+    <Outlet context ={[count, Setcount]}/>   
      </div>
   </>
       
--- src/pages/host/Dashboard.jsx
+++ src/pages/host/Dashboard.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import '../../App.css';
+import { useOutletContext } from "react-router-dom";
 
 
 
 function Dashboard() {
+const [count, Setcount] = useOutletContext();
+const increment = () => Setcount((count) = count + 1) ;
     return  <>  
     <div>
     <h1>host welcomes you</h1>
+    <button onClick={increment}>{count}</button>
     </div>
    
      </>
